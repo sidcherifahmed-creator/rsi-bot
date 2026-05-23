ede0e0b905e0cb36f3516f0f8106ae8059a748db
Mohamed Amine <sidcherifahmed@gmail.com>
2026-05-23 21:44:08 +0200

    fix token

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
-TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
+TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN", "8613977074:AAHgC5gZtmRuZugL8zF1mRiph4YZTWRxC8A")
 TG_CHAT_ID = int(os.getenv("TG_CHAT_ID", "-1003959930384"))
 BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
 BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
